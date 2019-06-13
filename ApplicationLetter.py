93a83a3041ef6e1ca577a8be4354d0f6dd4afae6
Thanh Tran <49013066+thanhtrannn@users.noreply.github.com>
2019-06-13 16:05:27 -0400

    Complete application with .exe

--- ApplicationLetter.py
+++ ApplicationLetter.py
@@ -1,4 +1,5 @@
-# Script uses selenium and chrome web driver to log into DRUPAL with provided credentials, then extract students MMFAP letters and rename them with appropriate standard. List of URL
+# Script uses selenium and chrome web driver to log into DRUPAL with provided credentials,
+# then extract students MMFAP letters and rename them with appropriate standard. List of URL
 # and student information is taken from Calculated_Table extraction
 # Thanh Tran - June 3, 2019
 
@@ -22,65 +23,60 @@ dataDict = {}
 pathSelected = False
 
 # GUI Section
-# quit GUI and run program
-def runExtraction():
-    root.quit()
-    
+
 # used to select folder for letters to be extracted in
 def browse_button():
-    global path
     path = filedialog.askdirectory(title='Select a folder')
-    global pathSelected
-    pathSelected = True
-    folderString.set(path)
-    path = path.replace("/", "\\")
+    if path is not None:
+        global pathSelected
+        folderString.set(path.replace("/", "\\"))
+        pathSelected = True
+    
 # used to select calculated file to be used in script
 def filebrowse_button():
     global file
-    file = filedialog.askopenfile(parent=root, mode='rb', title='Select a file')
-    fileString.set(file.name)
+    file = filedialog.askopenfile(parent=root, mode='rb', title='Select a file', filetypes=[('Excel file', '*.xlsx;*.xlsm;*xls')])
+    if file is not None:
+        fileString.set(file.name)
     
 root = Tk()
 root.title('MMFAP Application Letter Extraction')
-root.geometry("600x400")
 
-urlLabel = Label(root, text='DRUPAL login URL: ').grid(row=0, padx=10, pady=5)
-usernameLabel = Label(root, text='DRUPAL username: ').grid(row=1, padx=10, pady=5)
-passwordLabel = Label(root, text='DRUPAL password: ').grid(row=2,padx=10, pady=5)
-folderLabel = Label(root, text='Extract Letters Into: ').grid(row=3, padx=10, pady=5)
-fileLabel = Label(root, text='Upload Calculated File: ').grid(row=4, padx=10, pady=5)
-timeLabel = Label(root, text='Seconds between downloads: ').grid(row=5, padx=10, pady=5)
-
-browseButton = Button(root, text='Browse', command=browse_button).grid(row=3, column=2, pady= 5, padx=10)
-browseFileButton = Button(root, text='Browse', command=filebrowse_button).grid(row=4, column=2)
-startButton = Button(root, text='Start', command=runExtraction, width = 16).grid(row=6, column=1, pady=10, padx=10)
-# set strings for entry value
+# set Labels
+Label(root, text='MMFAP Application Letter Extraction', font=('Aria', 12, 'bold')).grid(row=0, padx=10, pady=20, columnspan=3)
+Label(root, text='DRUPAL login URL: ').grid(row=1, padx=10, pady=10, sticky="e")
+Label(root, text='DRUPAL username: ').grid(row=2, padx=10, pady=10, sticky="e")
+Label(root, text='DRUPAL password: ').grid(row=3,padx=10, pady=10, sticky="e")
+Label(root, text='Extract Letters To: ').grid(row=4, padx=10, pady=10, sticky="e")
+Label(root, text='Calculated File: ').grid(row=5, padx=10, pady=10, sticky="e")
+Label(root, text='Seconds between downloads: ').grid(row=6, padx=10, pady=5, sticky="e")
+# set Buttons
+browseButton = Button(root, text='Browse', command=browse_button, width = 15).grid(row=4, column=2, pady= 5, padx=10)
+browseFileButton = Button(root, text='Browse', command=filebrowse_button, width = 15).grid(row=5, column=2)
+startButton = Button(root, text='Start', command=root.quit, width = 15, bg="medium sea green", fg="white").grid(row=7, column=1, pady=20, padx=10)
+# set Variables
 drupalURLString = StringVar()
 drupalURLString.set('https://www.mohawkcollege.ca/user/login')
 folderString = StringVar()
 fileString = StringVar()
 timeDLString = IntVar()
 timeDLString.set(3)
+username = StringVar()
+password = StringVar()
+# set Fields
+Entry(root, textvariable=drupalURLString, width=50).grid(row=1, column=1)
+Entry(root, width=50, textvariable=username).grid(row=2, column=1)
+Entry(root, width=50, textvariable=password).grid(row=3, column=1)
+Entry(root, width=50, state='readonly', textvariable=folderString).grid(row=4, column=1)
+Entry(root, width=50, state='readonly', textvariable=fileString).grid(row=5, column=1)
+Entry(root, textvariable=timeDLString, width=25).grid(row=6, column=1, sticky='w')
 
-timeDL = Entry(root, textvariable=timeDLString, width=25)
-drupalURL = Entry(root, state='readonly', textvariable=drupalURLString, width=50)
-username = Entry(root, width=50)
-password = Entry(root, width=50)
-folder = Entry(root, width=50, state='readonly', textvariable=folderString)
-file = Entry(root, width=50, state='readonly', textvariable=fileString)
-
-drupalURL.grid(row=0, column=1)
-username.grid(row=1, column=1)
-password.grid(row=2, column=1)
-folder.grid(row=3, column=1)
-file.grid(row=4, column=1)
-timeDL.grid(row=5, column=1)
 root.mainloop()
 
 # make directory to hold extract
-#path = os.getcwd() + '\\' + folder
-#if not os.path.exists(path):
-#    os.makedirs(path)
+# path = os.getcwd() + '\\' + folder
+# if not os.path.exists(path):
+#     os.makedirs(path)
     
 if getattr(sys, 'frozen', False) :
     # running in a bundle
@@ -90,16 +86,13 @@ else:
     base_dir = os.path.dirname(os.path.abspath(__file__))
 
 chromedriver_path = os.path.join(base_dir, 'chromedriver')
-
-# Create folder in current directory to hold letters if folder was not selected
+# create folder in current directory to hold letters if folder was not selected
 if pathSelected is False:
-    global path
-    path = os.getcwd() + '\\' + 'Application Letters'
-    if not os.path.exists(path):
-        os.makedirs(path)
+    folderString.set(os.getcwd() + '\\Application Letters')
+    if not os.path.exists(folderString.get()):
+        os.makedirs(folderString.get())
 
 # open calculated file
-#file =  "Calculated Table - Thanh's Test.xlsx"
 try:
     if file is not None:
         wb_obj = load_workbook(filename = file)
@@ -113,7 +106,7 @@ except:
 
 # chrome driver settings
 chrome_options = webdriver.ChromeOptions()
-prefs = {'download.default_directory' : path}
+prefs = {'download.default_directory' : folderString.get()}
 chrome_options.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(chromedriver_path,chrome_options=chrome_options)
 
@@ -135,7 +128,7 @@ for heading in dataDict[next(iter(dataDict))]:
     count += 1
 
 # begin initial login of DRUPAL
-driver.get(drupalURL.get())
+driver.get(drupalURLString.get())
 try:
     ui.WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id('edit-name'))
     driver.find_element_by_id("edit-name").click()
@@ -152,31 +145,31 @@ except:
     driver.quit()
     messagebox.showinfo("Error", "Login is not accepted")
     exit()
-#print(timeDL.get()) 
+
 # cycle through records
 for x in dataDict:
     # ignore empty cells
     if dataDict[x][appLetterCol] is not None and 'https' in dataDict[x][appLetterCol]:
         driver.get(dataDict[x][appLetterCol])
         # add timer to allow for download to complet
-        time.sleep(int(timeDL.get()))
+        time.sleep(int(timeDLString.get()))
         # get exacted downloaded file name
         file_name = dataDict[x][appLetterCol]
         file_name = file_name.split("/")
         file_name = urllib.parse.unquote(file_name[len(file_name) - 1])
-        file_to_rename = os.path.join(path, dataDict[x][firstNameCol] + "." + dataDict[x][lastNameCol] + "." + str(dataDict[x][studentNumCol]) +".pdf")
+        file_to_rename = os.path.join(folderString.get(), dataDict[x][firstNameCol] + "." + dataDict[x][lastNameCol] + "." + str(dataDict[x][studentNumCol]) +".pdf")
         #search for the download and rename
-        for filename in os.listdir(path):
+        for filename in os.listdir(folderString.get()):
             # check for file renaming, if file exist then rename with appending number
             if filename.startswith(file_name) and not os.path.exists(file_to_rename):
-                    os.rename(os.path.join(path, filename), file_to_rename)
+                    os.rename(os.path.join(folderString.get(), filename), file_to_rename)
             elif filename.startswith(file_name) and os.path.exists(file_to_rename):
                 trigger = True
                 count = 1
                 # continue searching for duplicates
                 while trigger:
-                    if not os.path.exists(os.path.join(path, dataDict[x][firstNameCol] + "." + dataDict[x][lastNameCol] + "." + str(dataDict[x][studentNumCol]) + "_" + str(count) + ".pdf")):
-                        os.rename(os.path.join(path, filename), os.path.join(path, dataDict[x][firstNameCol] + "." + dataDict[x][lastNameCol] + "." + str(dataDict[x][studentNumCol]) + "_" + str(count) + ".pdf"))
+                    if not os.path.exists(os.path.join(folderString.get(), dataDict[x][firstNameCol] + "." + dataDict[x][lastNameCol] + "." + str(dataDict[x][studentNumCol]) + "_" + str(count) + ".pdf")):
+                        os.rename(os.path.join(folderString.get(), filename), os.path.join(folderString.get(), dataDict[x][firstNameCol] + "." + dataDict[x][lastNameCol] + "." + str(dataDict[x][studentNumCol]) + "_" + str(count) + ".pdf"))
                         trigger = False
                     count = count + 1
             else:
